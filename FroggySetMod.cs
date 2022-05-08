8bfaf4554df31c85be0b5c9ce3b2f6af879fd036
Patrick Emery <patrick.emery@outlook.com>
2022-05-08 08:26:23 -0400

    Improve furniture graphics
    Signed-off-by: Patrick Emery <patrick.emery@outlook.com>

--- FroggySetMod.cs
+++ FroggySetMod.cs
@@ -14,65 +14,67 @@ using TinyLife.Objects;
 using TinyLife.Utilities;
 using TinyLife.World;
 
-namespace FroggySetMod;
+namespace FroggySetMod {
 
-public class FroggySetMod : Mod {
+    public class FroggySetMod : Mod {
 
-    public static Logger Logger { get; private set; }
+        public static Logger Logger { get; private set; }
+        public static ModInfo Info;
 
-    public override string Name => "Froggy Set";
-    public override string Description => "Not just Froggy Chair but Lily-Pad Table too (:";
-    public override TextureRegion Icon => UI[0, 0];
+        public override string Name => "Froggy Set";
+        public override string Description => "Not just Froggy Chair but Lily-Pad Table too (:";
+        public override TextureRegion Icon => UI[0, 0];
 
-    private UniformTextureAtlas LUT;
-    public TextureRegion BaseFroggy => LUT[0, 0];
-    public TextureRegion PinkFroggy => LUT[1, 0];
-    public TextureRegion YellowFroggy => LUT[0, 1];
+        private UniformTextureAtlas LUT;
+        public TextureRegion FroggyBack => LUT[0, 0];
+        public TextureRegion FroggyPad => LUT[0, 1];
 
-    public ColorScheme BaseFroggyColors => ColorScheme.Create(BaseFroggy);
-    public ColorScheme PinkFroggyColors => ColorScheme.Create(PinkFroggy);
-    public ColorScheme YellowFroggyColors => ColorScheme.Create(YellowFroggy);
+        public ColorScheme FroggyBackColors => ColorScheme.Load(FroggyBack);
+        public ColorScheme FroggyPadColors => ColorScheme.Load(FroggyPad);
 
-    private UniformTextureAtlas UI;
+        private UniformTextureAtlas UI;
 
-    public override void AddGameContent(GameImpl game, ModInfo info) {
-        FurnitureType.Register(new FurnitureType.TypeSettings(
-            "FroggySetMod.LilyPadTable", 
-            new Point(2, 2), 
-            ObjectCategory.Table, 
-            150, 
-            BaseFroggyColors,
-            PinkFroggyColors,
-            YellowFroggyColors
-        ) {
-            ConstructedType = typeof(LilyPadTable),
-            Icon = Icon,
-            ObjectSpots = ObjectSpot.TableSpots(new Point(2, 2)).ToArray()
-        });
-        FurnitureType.Register(new FurnitureType.TypeSettings(
-            "FroggySetMod.FroggyChair", 
-            new Point(1, 1), 
-            ObjectCategory.Chair, 
-            75, 
-            BaseFroggyColors,
-            PinkFroggyColors,
-            YellowFroggyColors
-        ) {
-            ConstructedType = typeof(FroggyChair),
-            Icon = Icon,
-            ObjectSpots = {}
-        });
+        public override void AddGameContent(GameImpl game, ModInfo info) {
+            FurnitureType.Register(new FurnitureType.TypeSettings(
+                $"{info.Id}.LilyPadTable", 
+                new Point(1, 1), 
+                ObjectCategory.Table, 
+                150, 
+                ColorScheme.White,
+                FroggyBackColors,
+                FroggyPadColors
+            ) {
+                ConstructedType = typeof(LilyPadTable),
+                Icon = Icon,
+                ObjectSpots = ObjectSpot.TableSpots(new Point(1, 1)).ToArray()
+            });
+            FurnitureType.Register(new FurnitureType.TypeSettings(
+                $"{info.Id}.FroggyChair", 
+                new Point(1, 1), 
+                ObjectCategory.Chair, 
+                75, 
+                FroggyBackColors,
+                FroggyPadColors,
+                ColorScheme.White
+            ) {
+                ConstructedType = typeof(FroggyChair),
+                Icon = Icon,
+                ObjectSpots = {},
+                ActionSpots = {}
+            });
+        }
+
+        public override void Initialize(Logger logger, RawContentManager content, RuntimeTexturePacker texturePacker, ModInfo info) {
+            Logger = logger;
+            Info = info;
+
+            texturePacker.Add(content.Load<Texture2D>("UiTextures"), r => UI = new UniformTextureAtlas(r, 1, 1));
+            texturePacker.Add(content.Load<Texture2D>("FroggySetLUT"), r => LUT = new UniformTextureAtlas(r, 1, 8));
+        }
+
+        public override IEnumerable<string> GetCustomFurnitureTextures(ModInfo info) {
+            yield return "FroggySetFurniture";
+        }
+
     }
-
-    public override void Initialize(Logger logger, RawContentManager content, RuntimeTexturePacker texturePacker, ModInfo info) {
-        Logger = logger;
-
-        texturePacker.Add(content.Load<Texture2D>("UiTextures"), r => UI = new UniformTextureAtlas(r, 16, 16));
-        texturePacker.Add(content.Load<Texture2D>("FroggySetLUT"), r => LUT = new UniformTextureAtlas(r, 4, 4));
-    }
-
-    public override IEnumerable<string> GetCustomFurnitureTextures(ModInfo info) {
-        yield return "FroggySetFurniture";
-    }
-
 }
