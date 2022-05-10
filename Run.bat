8d4877cdcb4e3ef67e19c0d5a1c444b8a88f38a8
Patrick Emery <patrick.emery@outlook.com>
2022-05-10 09:59:33 -0400

    Avoid issues with old content
    Signed-off-by: Patrick Emery <patrick.emery@outlook.com>

--- Run.bat
+++ Run.bat
@@ -4,8 +4,11 @@ cd /d "%~dp0"
 rem build the mod
 dotnet build || exit /b
 rem copy the mod to the mods folder
+move "%LOCALAPPDATA%/Tiny Life/Mods" "%LOCALAPPDATA%/Tiny Life/ModsBackup"
 robocopy ./bin/Debug/net6.0/ "%LOCALAPPDATA%/Tiny Life/Mods" /e /is
 rem run the game
 set /p dir=<"%LOCALAPPDATA%/Tiny Life/GameDir"
 cd /d %dir%
 "Tiny Life.exe" -v --skip-splash --skip-preloads
+del "%LOCALAPPDATA%/Tiny Life/Mods"
+move "%LOCALAPPDATA%/Tiny Life/ModsBackup" "%LOCALAPPDATA%/Tiny Life/Mods"
