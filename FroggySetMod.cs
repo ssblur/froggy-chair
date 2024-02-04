8675a26414f2d93de0a7f238b6aa31b2f3965238
Patrick Hallbick <patrick.hallbick@outlook.com>
2024-02-04 13:24:27 -0500

    Test for 0.39.1

--- FroggySetMod.cs
+++ FroggySetMod.cs
@@ -20,7 +20,7 @@ namespace FroggySetMod {
 
         public override string Name => "Froggy Set";
         public override string Description => "Not just Froggy Chair but Lily-Pad Table too (:";
-        public override string TestedVersionRange => "[0.38.0,0.39.0]";
+        public override string TestedVersionRange => "[0.38.0,0.39.1]";
 
         public override TextureRegion Icon => UI[0, 0];
 
